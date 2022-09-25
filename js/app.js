8b2b4600134d05b73bba3335c70518570aac4b76
HeberFG <99307841+HeberFG@users.noreply.github.com>
2022-09-25 13:19:24 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
+    
+    let thisUri = window.location;
+    if(thisUri.toString().includes('127.0.0.1')){
+        navigator.serviceWorker.register('/sw.js');
+    }else{
+        navigator.serviceWorker.register('/heberfg.github.io/sw.js');
+    }
 }
